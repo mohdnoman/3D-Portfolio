ae164a7ccb8b6182b61d44fb9347bf3e27be0d8c
mohd noman <mohdnoman2751@gmail.com>
2024-01-26 20:55:14 +0530

    created home page with all animations.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ function Navbar() {
     <header className="header">
       <NavLink
         to={"/"}
-        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+        className="w-30 px-3 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
       >
-        <p className="blue-gradient_text">AH</p>
+        <p className="blue-gradient_text">Noman's Island</p>
       </NavLink>
       <nav className="flex text-lg gap-7 font-medium">
         <NavLink
